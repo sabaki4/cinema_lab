7bc5a571fe81392006e8885ebc37317029ce2002
Saba Kia <skia6589@gmail.com>
2024-02-27 16:49:12 +0000

    movie model done

---
+++ src/main/java/com/example/cinema_lab/models/Movie.java
@@ -0,0 +1,59 @@
+package com.example.cinema_lab.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private long id;
+
+        @Column(name = "title")
+        private String title;
+
+        @Column(name = "rating")
+        private int rating;
+
+        @Column(name = "duration")
+        private int duration;
+
+        public Movie(String title){
+            this.title = title;
+            this.rating = 0;
+            this.duration = 0;
+        }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
